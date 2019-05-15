19e871a0b879f74f2268edc04ad300fce96a29fd
katie dwyer <katie.dwyer@techelevator.com>
2019-05-15 11:35:37 -0400

    03 Expressions student-lecture completed during class

---
+++ module-1/03_Expressions/student-lecture-classtime/dotnet/Lecture/05_ReturnNameOfLanguage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+        /*
+        5. This method should return the language that you're learning (C#). 
+            Change the code so it does that.
+            TOPIC: Return Types
+        */
+        public string ReturnNameOfLanguage()
+        {
+            string myLang = "C#";
+            return myLang; 
+        }
+    }
+}
