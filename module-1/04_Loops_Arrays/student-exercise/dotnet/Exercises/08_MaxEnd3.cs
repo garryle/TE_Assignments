e4f3fe0ffc8f16ce095b009d9a748805c62aea98
Garry <legp567@gmail.com>
2019-05-16 23:15:56 -0400

    Unfinished Assignment 4

--- module-1/04_Loops_Arrays/student-exercise/dotnet/Exercises/08_MaxEnd3.cs
+++ module-1/04_Loops_Arrays/student-exercise/dotnet/Exercises/08_MaxEnd3.cs
@@ -16,11 +16,20 @@ namespace Exercises
          MaxEnd3([2, 11, 3]) → [3, 3, 3]
          */
         public int[] MaxEnd3(int[] nums)
-        { if (nums[0] > nums[2])
-                return new int[] { nums[0], nums[0], nums[0] };
-
-          else (nums[0] < nums[2])
-                return new int[] { nums[2], nums[2], nums[2] };
+        {
+            if (nums[0] >= nums[nums.Length - 1])
+            {
+                nums[0] = nums[0];
+                nums[1] = nums[0];
+                nums[2] = nums[0];
+            }
+            else if (nums[0] <= nums[nums.Length - 1])
+            {
+                nums[0] = nums[nums.Length - 1];
+                nums[1] = nums[nums.Length - 1];
+                nums[2] = nums[nums.Length - 1];
+            }
+                return new int[] { nums[0], nums[1], nums[2]};
         }
 
     }
