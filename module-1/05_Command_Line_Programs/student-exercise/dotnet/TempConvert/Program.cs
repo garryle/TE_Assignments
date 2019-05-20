1c463ccdecef4e2ab10ebf54f6c93a4fc53130aa
Garry <legp567@gmail.com>
2019-05-19 23:56:03 -0400

    Unfinished Binary

--- module-1/05_Command_Line_Programs/student-exercise/dotnet/TempConvert/Program.cs
+++ module-1/05_Command_Line_Programs/student-exercise/dotnet/TempConvert/Program.cs
@@ -6,10 +6,26 @@ namespace TempConvert
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter the length.");
-            Console.ReadLine();
 
-            Console.WriteLine("Is the measurement in (m)eter, or(f)eet ?");
+            int tempInput;
+            string scaleInput;
+
+            Console.WriteLine("Please enter the temperature.");
+            tempInput = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Is the temperature in (C)elcius, or (F)arenheit?");
+            scaleInput = Console.ReadLine();
+
+            if (scaleInput.Equals("C"))
+            {
+                int tempF = (int)(tempInput * 1.8 + 32);
+                Console.WriteLine(tempInput + "C is " + tempF + "F.");
+            }
+            else
+            {
+                int tempC = (int)((tempInput - 32) / 1.8);
+                Console.WriteLine(tempInput + "F is " + tempC + "C.");
+            }
         }
     }
  
