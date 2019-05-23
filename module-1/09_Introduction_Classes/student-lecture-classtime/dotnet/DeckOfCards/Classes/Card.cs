a5c50533807566c64c80694ea5c780c246f2d379
katie dwyer <katie.dwyer@techelevator.com>
2019-05-23 12:09:44 -0400

    09 Intro to Classes class lecture and student exercises

---
+++ module-1/09_Introduction_Classes/student-lecture-classtime/dotnet/DeckOfCards/Classes/Card.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DeckOfCards.Classes
+{
+    public class Card
+    {
+        /// <summary>
+        /// The numeric value of the card. This will be 11 for Jacks, 12 for Queen, 13 for king, 1 for ace
+        /// </summary>
+        public int Value { get; }
+
+        /// <summary>
+        /// The card suit
+        /// </summary>
+        public string Suit { get; }
+
+        /// <summary>
+        /// Derived property based on the Suit
+        /// </summary>
+        public string Color
+        {
+            get
+            {
+                if (Suit == "Hearts" || Suit == "Diamonds")
+                {
+                    return "Red";
+                }
+                else
+                {
+                    return "Black";
+                }
+            }
+        }
+
+        public bool IsFaceUp { get; private set; }
+
+        /// <summary>
+        /// Constructor to create a new card face down
+        /// </summary>
+        /// <param name="value"> the numeric value of the card 11 for jack</param>
+        /// <param name="suit">Hearts, Diamonds, Spades, Clubs</param>
+        public Card (int value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+            IsFaceUp = false; 
+        }
+
+        /// <summary>
+        /// Constructor that lets the user specify value, suit and is face up
+        /// </summary>
+        /// <param name="value">numeric value of the card 11 for jacks</param>
+        /// <param name="suit">Hearts, Diamonds, Spades, Clubs</param>
+        /// <param name="isFaceUp">true for face up, false for face down</param>
+        public Card(int value, string suit, bool isFaceUp)
+        {
+            Value = value;
+            Suit = suit;
+            IsFaceUp = isFaceUp;
+        }
+
+        /// <summary>
+        /// Toggle faceup to face down and face down to face up
+        /// </summary>
+        public void Flip()
+        {
+            //Console.WriteLine("in flip with no parameter");
+            IsFaceUp = !IsFaceUp; 
+        }
+
+        public void Flip(int numberOfTimes)
+        {
+            //Console.WriteLine("in flip with parameter");
+            for (int i=0; i<numberOfTimes;i++)
+            {
+                Flip(); 
+            }
+        }
+
+        public override string ToString()
+        {
+            string cardStr = "IDK";
+            if (IsFaceUp)
+            {
+                cardStr = "";
+
+                if (Value > 1 && Value <= 10)
+                {
+                    cardStr += Value;
+                }
+                else
+                {
+                    switch (Value)
+                    {
+                        case 11:
+                            cardStr += "Jack";
+                            break;
+                        case 12:
+                            cardStr += "Queen";
+                            break;
+                        case 13:
+                            cardStr += "King";
+                            break;
+                        case 1:
+                            cardStr += "Ace";
+                            break;
+                    }
+
+                }
+                cardStr += " of " + Suit; 
+            }
+            return cardStr;
+        }
+    }
+}
---
+++ module-1/09_Introduction_Classes/student-lecture-classtime/dotnet/DeckOfCards/Program.cs
@@ -0,0 +1,87 @@
+﻿using DeckOfCards.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Let's store all of our cards in a list
+            // CODE GOES HERE
+            List<Card> myDeck = new List<Card>(); 
+
+
+            while (true)
+            {
+                Console.WriteLine("What do you want to do? ");
+                Console.WriteLine("1. Create a new card.");
+                Console.WriteLine("2. Display all of the cards.");
+                Console.WriteLine("3. Flip all of the cards.");
+                Console.WriteLine("Q. Quit");
+
+                string input = Console.ReadLine();
+
+                if (input == "1")
+                {
+                    // Get the value for the new card
+                    Console.Write("What is the value of the card (1-13): ");
+                    int value = int.Parse(Console.ReadLine());
+
+                    // Get the suit for the new card
+                    Console.Write("What suit does the card have (Hearts, Diamonds, Clubs, Spades): ");
+                    string suit = Console.ReadLine();
+
+                    // Is the card face up or face down
+                    Console.Write("Is the card face up (True/False): ");
+                    bool isFaceUp = bool.Parse(Console.ReadLine());
+
+                    // Create the card and add to the list
+                    Card myCard = new Card(value, suit, isFaceUp);
+                    myDeck.Add(myCard);    
+                    
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Displaying all of the cards.");
+
+                    // Loop through each of the cards
+                    foreach(Card c in myDeck)
+                    {
+                        Console.WriteLine(c.ToString());
+                    }
+                    
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("How many times?");
+                    int numTimes = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Flipping the cards.");
+                    
+                    // Loop through each of the cards and flip them
+                    foreach(Card card in myDeck)
+                    {
+                        if (numTimes == 1)
+                        {
+                            card.Flip();
+                        }
+                        else
+                        {
+                            card.Flip(numTimes);
+                        }
+                    }
+                    
+                }
+                else if (input == "Q")
+                {
+                    break;
+                }
+
+                // Wait for user to press enter and clear screen.
+                Console.ReadLine();
+                Console.Clear();
+            }
+        }
+    }
+}
