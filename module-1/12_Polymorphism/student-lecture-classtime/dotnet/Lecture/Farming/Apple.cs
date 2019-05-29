d13663b3c683e3738dba31f67c752b33becf7a83
katie dwyer <katie.dwyer@techelevator.com>
2019-05-29 10:48:00 -0400

    12 classtime lecture and student exercise

---
+++ module-1/12_Polymorphism/student-lecture-classtime/dotnet/Lecture/Farming/Apple.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Apple : ISellable
+    {
+        public double Price { get; set; }
+        public string Color { get; set;  }
+    }
+}
---
+++ module-1/12_Polymorphism/student-lecture-classtime/dotnet/Lecture/Farming/MachoManRandySavage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class MachoManRandySavage : FarmAnimal
+    {
+        public MachoManRandySavage():base("MACHOMANRANDYSAVAGE")
+        {
+            Price = 10; 
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "OH YEAH";
+        }
+
+        public override string MakeSoundTwice()
+        {
+            return "SNAP INTO A SLIM JIM";
+        }
+    }
+}
