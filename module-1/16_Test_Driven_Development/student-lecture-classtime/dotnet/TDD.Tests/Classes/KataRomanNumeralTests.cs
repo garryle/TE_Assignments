d3fec52f6c453341e3bc99842c0d49edd2d7c660
katie dwyer <katie.dwyer@techelevator.com>
2019-06-05 11:11:27 -0400

    16 TDD classtime lecture

---
+++ module-1/16_Test_Driven_Development/student-lecture-classtime/dotnet/TDD.Tests/Classes/KataRomanNumeralTests.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDD.Classes;
+
+namespace TDD.Tests.Classes
+{
+    [TestClass]
+    public class KataRomanNumeralTests
+    {
+        KataRomanNumeral kata;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            kata = new KataRomanNumeral(); 
+        }
+
+        [TestMethod]
+        public void Test_Ones()
+        {
+            Assert.AreEqual("I", kata.GetRomanNumeral(1));
+            Assert.AreEqual("II", kata.GetRomanNumeral(2));
+            Assert.AreEqual("III", kata.GetRomanNumeral(3));
+        }
+
+        [TestMethod]
+        public void Test_Fives()
+        {
+            Assert.AreEqual("V", kata.GetRomanNumeral(5));
+            Assert.AreEqual("VI", kata.GetRomanNumeral(6));
+            Assert.AreEqual("VII", kata.GetRomanNumeral(7));
+        }
+
+        [TestMethod]
+        public void Test_Tens()
+        {
+            Assert.AreEqual("X", kata.GetRomanNumeral(10));
+            Assert.AreEqual("XI", kata.GetRomanNumeral(11));
+            Assert.AreEqual("XV", kata.GetRomanNumeral(15));
+            Assert.AreEqual("XVII", kata.GetRomanNumeral(17));
+            Assert.AreEqual("XXXVI", kata.GetRomanNumeral(36));
+        }
+
+        [TestMethod]
+        public void Test_Fifties()
+        {
+            Assert.AreEqual("L", kata.GetRomanNumeral(50));
+            Assert.AreEqual("LXVI", kata.GetRomanNumeral(66));
+            Assert.AreEqual("LXXXVII", kata.GetRomanNumeral(87));         
+        }
+
+
+        [TestMethod]
+        public void Test_Rest()
+        {
+            Assert.AreEqual("C", kata.GetRomanNumeral(100));
+            Assert.AreEqual("CLXVI", kata.GetRomanNumeral(166));
+            Assert.AreEqual("D", kata.GetRomanNumeral(500));
+            Assert.AreEqual("M", kata.GetRomanNumeral(1000));
+            Assert.AreEqual("MDCCLXVI", kata.GetRomanNumeral(1766)); 
+        }
+
+        [TestMethod]
+        public void Test_Subtraction()
+        {
+            Assert.AreEqual("IV", kata.GetRomanNumeral(4));
+            Assert.AreEqual("IX", kata.GetRomanNumeral(9));
+            Assert.AreEqual("XXIX", kata.GetRomanNumeral(29));
+            Assert.AreEqual("XL", kata.GetRomanNumeral(40));
+            Assert.AreEqual("XC", kata.GetRomanNumeral(90));
+            Assert.AreEqual("CD", kata.GetRomanNumeral(400));
+            Assert.AreEqual("CM", kata.GetRomanNumeral(900));
+            
+        }
+
+        [TestMethod]
+        public void Test_Random()
+        {          
+            Assert.AreEqual("MMCMXCIX", kata.GetRomanNumeral(2999));
+            Assert.AreEqual("CDXLIV", kata.GetRomanNumeral(444));
+        }
+    }
+}
---
+++ module-1/16_Test_Driven_Development/student-lecture-classtime/dotnet/TDD/Classes/KataRomanNumeral.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Classes
+{
+    public class KataRomanNumeral
+    {
+        public string GetRomanNumeral(int number)
+        {
+            string result = "";
+
+            result += Convert(ref number, 1000, "M");
+            result += ReplaceSubtraction(ref number, 900, "CM"); 
+            result += Convert(ref number, 500, "D");
+            result += ReplaceSubtraction(ref number, 400, "CD"); 
+            result += Convert(ref number, 100, "C");
+            result += ReplaceSubtraction(ref number, 90, "XC");
+            result += Convert(ref number, 50, "L");
+            result += ReplaceSubtraction(ref number, 40, "XL"); 
+            result += Convert(ref number, 10, "X");
+            result += ReplaceSubtraction(ref number, 9, "IX"); 
+            result += Convert(ref number, 5, "V");
+            result += ReplaceSubtraction(ref number,4,"IV" );            
+            result += Convert(ref number, 1, "I");
+
+            return result;
+
+        }
+
+        private static string ReplaceSubtraction(ref int number,int replaceNumber, string strReplacement)
+        {
+            string result = "";
+            if (number >= replaceNumber)
+            {
+                result += strReplacement;
+                number = number - replaceNumber;
+            }
+            return result; 
+        }
+
+        private static string Convert(ref int number, int digit, string digitValue)
+        {
+            string result = "";
+            while (number >= digit)
+            {
+                result += digitValue;
+                number = number - digit;
+            }
+            return result; 
+        }
+    }
+}
