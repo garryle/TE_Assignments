3662e4c098c7f0e65fa695516fddb78697a22b44
Chris Rupp <chris@techelevator.com>
2019-06-13 11:28:52 -0400

    insert...

--- module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-ms sql.sql
+++ module-2/04_INSERT_UPDATE_and_DELETE/student-lecture/lecture-ms sql.sql
@@ -1,38 +1,83 @@
 -- INSERT
 
 -- 1. Add Klingon as a spoken language in the USA
--- 2. Add Klingon as a spoken language in Great Britain
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'Klingon', 0, 99.9);
 
+-- 2. Add Klingon as a spoken language in Great Britain
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('GBR', 'Klingon', 0, 98.9);
+
+Select * From countrylanguage Where countrycode In ('USA', 'GBR');
 
 -- UPDATE
 
 -- 1. Update the capital of the USA to Houston
--- 2. Update the capital of the USA to Washington DC and the head of state
+Select * From country Where code = 'USA'
 
+Select * From city Where id = 3796
+
+UPDATE country SET capital = (Select id From city Where name = 'Houston')
+WHERE code = 'USA';
+
+-- 2. Update the capital of the USA to Washington DC and the head of state
+Select id From city Where name = 'Washington'
+Select * From country Where code = 'USA'
+
+Update country Set capital = (Select id From city Where name = 'Washington')
+Where code = 'USA'
 
 -- DELETE
 
 -- 1. Delete English as a spoken language in the USA
--- 2. Delete all occurrences of the Klingon language 
+Select * From countrylanguage
+Where countrycode = 'USA' And language = 'English'
 
+DELETE FROM countrylanguage
+Where countrycode = 'USA' And language = 'English'
+
+-- 2. Delete all occurrences of the Klingon language 
+Select * From countrylanguage
+Where language = 'Klingon'
+
+DELETE FROM countrylanguage
+Where language = 'Klingon'
 
 -- REFERENTIAL INTEGRITY
 
 -- 1. Try just adding Elvish to the country language table.
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('MID', 'Elvish', 0, 99.9);
 
 -- 2. Try inserting English as a spoken language in the country ZZZ. What happens?
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('ZZZ', 'English', 0, 99.9);
 
 -- 3. Try deleting the country USA. What happens?
-
+Delete From country Where code = 'USA'
+Select * From country Where code = 'USA'
+Select * From city Where countrycode = 'USA'
+Select * From countrylanguage Where countrycode = 'USA'
 
 -- CONSTRAINTS
 
 -- 1. Try inserting English as a spoken language in the USA
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'English', 0, 99.9); 
 
 -- 2. Try again. What happens?
+INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+VALUES ('USA', 'English', 0, 99.9);
 
 -- 3. Let's relocate the USA to the continent - 'Outer Space'
+Update country Set continent = 'Outer Space'
+Where code = 'USA'
 
+Select * From country Where code = 'USA'
+
+Select count(*) as 'Total', capital From country
+Group By capital
+Order By 'Total' Desc
 
 -- How to view all of the constraints
 
@@ -44,7 +89,20 @@ SELECT * FROM INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS
 -- TRANSACTIONS
 
 -- 1. Try deleting all of the rows from the country language table and roll it back.
+BEGIN TRANSACTION
+Select * From countrylanguage
+Delete From countrylanguage
+Select * From countrylanguage
+ROLLBACK TRANSACTION
+Select * From countrylanguage
 
--- 2. Try updating all of the cities to be in the USA and roll it back
+BEGIN TRANSACTION
+BEGIN TRY	
+	INSERT INTO countrylanguage (countrycode, language, isofficial, percentage)
+	VALUES ('USA', 'Klingon', 1, 100);
+	COMMIT TRANSACTION
+END TRY
 
--- 3. Demonstrate two different SQL connections trying to access the same table where one happens to be inside of a transaction but hasn't committed yet.
+BEGIN CATCH
+	ROLLBACK TRANSACTION
+END CATCH
