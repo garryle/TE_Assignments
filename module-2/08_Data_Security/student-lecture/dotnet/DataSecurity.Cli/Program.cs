a573450d93c3a320397a25bd99ea31ac3f88d175
Chris Rupp <chris@techelevator.com>
2019-06-19 10:13:01 -0400

    security

---
+++ module-2/08_Data_Security/student-lecture/dotnet/DataSecurity.Cli/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Security.BusinessLogic;
+using Security.DAO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DataSecurity.Cli
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TestAuthentication();
+            //TestEncryption();
+        }
+
+        public static void TestAuthentication()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = builder.Build();
+
+            string connectionString = configuration.GetConnectionString("UserSecurityConnection");
+
+            IUserSecurityDAO db = new UserSecurityDAO(connectionString);
+            UserManager userMgr = new UserManager(db);
+            DataSecurityCLI cli = new DataSecurityCLI(userMgr);
+            cli.Run();
+        }
+
+        public static void TestEncryption()
+        {
+            Encryption.GenerateKeys("public.xml", "private.xml");
+            var text = Encryption.GetCipherText("Chris can teach!", "public.xml");
+            Console.WriteLine($"Encrypted: {text}");
+            text = Encryption.DecryptCipherText(text, "private.xml");
+            Console.WriteLine($"Decrypted: {text}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ module-2/08_Data_Security/student-lecture/dotnet/Security/DAO/IUserSecurityDAO.cs
@@ -0,0 +1,28 @@
+﻿using Security.Models.Database;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Security.DAO
+{
+    public interface IUserSecurityDAO
+    {
+        #region UserItem
+
+        int AddUserItem(UserItem item);
+        bool UpdateUserItem(UserItem item);
+        void DeleteUserItem(int userId);
+        UserItem GetUserItem(int userId);
+        UserItem GetUserItem(string username);
+        List<UserItem> GetUserItems();
+
+        #endregion
+
+        #region RoleItem
+
+        int AddRoleItem(RoleItem item);
+        List<RoleItem> GetRoleItems();
+
+        #endregion        
+    }
+}
