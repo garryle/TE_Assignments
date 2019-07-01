23eacf70ca689a8d7619dfd7dbef812914b5aca5
Chris Rupp <chris@techelevator.com>
2019-07-01 12:12:05 -0400

    get request

---
+++ module-3/06-Forms-and-Controllers-HTTP-GET/student-lecture/dotnet/Forms.Web/Models/CitySearchViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forms.Web.Models
+{
+    public class CitySearchViewModel
+    {
+        // The selected country
+        [DisplayName("Choose City:")]
+        public string City { get; set; }
+
+        // The choices the user can select from
+        public List<SelectListItem> Cities { get; } = new List<SelectListItem>();
+    }
+}
