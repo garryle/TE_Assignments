a8135db977089994aeb7530df0ef30fe0c16d154
Chris Rupp <chris@techelevator.com>
2019-07-03 13:19:57 -0400

    sessions

---
+++ module-3/08-Session-and-Flash-Scope/student-lecture-chris/dotnet/Lecture.Web/Models/AddCountryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture.Web.Models
+{
+    public class AddCountryViewModel
+    {
+        public string Name { get; set; }
+
+        public List<string> ExistingCountries { get; set; }
+    }
+}
