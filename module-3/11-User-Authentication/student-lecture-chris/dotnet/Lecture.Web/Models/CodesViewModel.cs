9bde90900da5bf013cbd28d73a1784b7694de0c8
Chris Rupp <chris@techelevator.com>
2019-07-09 13:56:38 -0400

    awesome code

---
+++ module-3/11-User-Authentication/student-lecture-chris/dotnet/Lecture.Web/Models/CodesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture.Web.Models
+{
+    public class CodesViewModel
+    {
+        public string CountryCode { get; set; }
+        public string CountryName { get; set; }
+        public string LaunchCode { get; set; }
+    }
+}
---
+++ module-3/11-User-Authentication/student-lecture-chris/dotnet/Lecture.Web/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lecture.Web.Models
+{
+    public class LoginViewModel
+    {
+        [DisplayName("Username")]
+        [Required]
+        public string Username { get; set; }
+
+        [DisplayName("Password")]
+        [Required]
+        public string Password { get; set; }
+    }
+}
